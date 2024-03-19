be075b9b3631a3532a53c618204f3a3b27b167e6
zerootwooooo <zanderarevalo20@gmail.com>
2024-03-20 04:35:42 +0800

    commit

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from "express";
+import User from "../models/user";
 
-const createCurrentUser = (req: Request, res: Response) => {
-  //user exist ? create user : call user account
+const createCurrentUser = async (req: Request, res: Response) => {
+  //user exist ? call user account : create user
+
+  try {
+    const { auth0Id } = req.body;
+    const existingUser = await User.findOne({ auth0Id });
+
+    if (existingUser) {
+      return res.status(200).send();
+    }
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error Creating user" });
+  }
 };
 
 export default {
