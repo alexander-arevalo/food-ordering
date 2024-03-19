9e6e2e921e4014c89a8e3ff36d08a2e60d27cd93
zerootwooooo <zanderarevalo20@gmail.com>
2024-03-20 04:10:07 +0800

    commit

---
+++ backend/src/controllers/MyUserController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+const createCurrentUser = (req: Request, res: Response) => {
+  //user exist ? create user : call user account
+};
+
+export default {
+  createCurrentUser,
+};
